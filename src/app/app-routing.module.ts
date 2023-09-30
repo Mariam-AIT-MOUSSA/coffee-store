a76a43493d97d465786b291fcf9d06b2b889b0b0
Mariam AIT MOUSSA <mariam15aitmoussa@gmail.com>
2023-09-30 14:50:31 +0100

    project architecture defined & needed components and files created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoffeesComponent } from './coffees/coffees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'coffee-list', component: CoffeesComponent},
+  {path: '', redirectTo: '/coffee-list', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoffeesComponent } from './coffees/coffees.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoffeesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
