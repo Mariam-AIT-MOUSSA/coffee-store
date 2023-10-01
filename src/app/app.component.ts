8662a216515d3bdf343bce76cc74f8b0a291a551
Mariam AIT MOUSSA <mariam15aitmoussa@gmail.com>
2023-10-01 22:34:10 +0100

    QUICKFIXS on style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { initCoffeeList } from './store/coffee-store/coffee.action';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { initCoffeeList } from './store/coffee-store/coffee.action';
 })
 export class AppComponent implements OnInit{
   
-  constructor(private store: Store){}
+  constructor(private store: Store, private matIconRegistry: MatIconRegistry, private sanitizer: DomSanitizer){
+    matIconRegistry.addSvgIcon('coffee-store-logo', this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/coffee-logo.svg'))
+  }
 
   ngOnInit(){
     this.store.dispatch(initCoffeeList()); 
